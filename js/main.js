8ae916313343cc1ea95c0da61f00badd05e56c8b
Luke Lancaster <luketlancaster@gmail.com>
2015-03-04 13:12:20 -0600

    copy scotts code to make following along easier

--- js/main.js
+++ js/main.js
@@ -1,14 +1,66 @@
-/*angular: true*/
-
 angular
-  .module('friends', [])
-  .controller('FriendsController', function (){
-    var friends = this;
+  .module('tas', [])
+  .controller('TasController', function () {
+    var vm = this;
 
-    friends.data = ['Luke', 'Jess', 'Phil', 'Johnnie'];
+    vm.data = [
+      {
+        name: 'TAdam',
+        nickName: 'Adam',
+        firstName: 'Adam',
+        lastName: 'Keesecker',
+        current: true
+      },
+      {
+        name: 'ZAdam',
+        nickName: 'Adad',
+        firstName: 'Zoe',
+        lastName: 'Ames',
+        current: false
+      },
+      {
+        name: 'JuAdam',
+        nickName: 'Adude',
+        firstName: 'Juan',
+        lastName: 'Rodriguez',
+        current: true
+      },
+      {
+        name: 'BrAdam',
+        nickName: 'Avoy',
+        firstName: 'Brian',
+        lastName: 'Hiatt',
+        current: false
+      },
+      {
+        name: 'BAdam',
+        nickName: 'Avast',
+        firstName: 'Adam',
+        lastName: 'Barnhard',
+        current: true
+      }
+    ];
 
-    friends.removeFriend = function(name) {
-      var index = friends.data.indexOf(name);
-      friends.data.splice(index, 1);
-    }
-  })
+    vm.newTA = {};
+
+    vm.addTA = function () {
+
+      vm.newTA.name = vm.newTA.name;
+      vm.newTA.firstName = vm.newTA.firstName;
+      vm.newTA.lastName = vm.newTA.lastName;
+      vm.newTA.nickName = vm.newTA.nickName;
+
+
+      vm.data.push(vm.newTA);
+
+      vm.newTA = {};
+
+    };
+
+
+    vm.removeTA = function (person) {
+      var index = vm.data.indexOf(person);
+      vm.data.splice(index, 1);
+    };
+
+  });
