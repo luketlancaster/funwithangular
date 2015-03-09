9a8a38fd1b353b2a56f8aefd7e82f87307cd7a74
Luke Lancaster <luketlancaster@gmail.com>
2015-03-09 12:09:41 -0500

    use  method to put and delete to firebase

--- js/main.js
+++ js/main.js
@@ -1,50 +1,13 @@
 angular
   .module('tas', [])
-  .controller('TasController', function ($scope) {
+  .controller('TasController', function ($scope, $http) {
     var vm = this;
 
-    vm.data = [
-      {
-        name: 'TAdam',
-        nickName: 'Adam',
-        firstName: 'Adam',
-        lastName: 'Keesecker',
-        current: true,
-        cohort: 5
-      },
-      {
-        name: 'ZAdam',
-        nickName: 'Adad',
-        firstName: 'Zoe',
-        lastName: 'Ames',
-        current: false,
-        cohort: 6
-      },
-      {
-        name: 'JuAdam',
-        nickName: 'Adude',
-        firstName: 'Juan',
-        lastName: 'Rodriguez',
-        current: true,
-        cohort: 6
-      },
-      {
-        name: 'BrAdam',
-        nickName: 'Avoy',
-        firstName: 'Brian',
-        lastName: 'Hiatt',
-        current: false,
-        cohort: 6
-      },
-      {
-        name: 'BAdam',
-        nickName: 'Avast',
-        firstName: 'Adam',
-        lastName: 'Barnhard',
-        current: true,
-        cohort: 6
-      }
-    ];
+    $http
+      .get('https://angularz.firebaseio.com/tas.json')
+      .success(function (data){
+        vm.data = data;
+      });
 
     vm.newTA = {};
 
@@ -56,22 +19,29 @@ angular
       vm.newTA.nickName = vm.newTA.nickName;
       vm.newTA.cohort = vm.newTA.cohort;
 
+      $http.post('https://angularz.firebaseio.com/tas.json', vm.newTA)
+        .success(function (data) {
+          vm.data[data.name] = vm.newTA;
+          _clearNewTA();
+        })
+      ;
 
-      vm.data.push(vm.newTA);
-
-      _clearNewTA();
 
     };
 
     function _clearNewTA() {
       vm.newTA = {};
-      $scope.newTA.$setPristine();
+      $scope.newTAForm.$setPristine();
     }
 
 
-    vm.removeTA = function (person) {
-      var index = vm.data.indexOf(person);
-      vm.data.splice(index, 1);
+    vm.removeTA = function (id) {
+      var url = 'https://angularz.firebaseio.com/tas/' + id +'/.json';
+       $http
+         .delete(url)
+         .success(function (){
+          delete vm.data[id];
+         });
     };
 
   });
